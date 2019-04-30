f5e1d9131cad858fb0b637aeac50cf8b467edfb6
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-04-29 18:40:07 -0700

    set-recv command

--- src/broker/types.rs
+++ src/broker/types.rs
@@ -1,5 +1,6 @@
 use crate::wallet::types::{Slate, TxProof};
 
+use common::config::Wallet713Config;
 use common::Error;
 use contacts::Address;
 
@@ -20,7 +21,7 @@ pub trait Subscriber {
 
 pub trait SubscriptionHandler: Send {
     fn on_open(&self);
-    fn on_slate(&self, from: &Address, slate: &mut Slate, proof: Option<&mut TxProof>);
+    fn on_slate(&self, from: &Address, slate: &mut Slate, proof: Option<&mut TxProof>, Option<Wallet713Config>);
     fn on_close(&self, result: CloseReason);
     fn on_dropped(&self);
     fn on_reestablished(&self);
