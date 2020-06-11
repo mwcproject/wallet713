1ba727de4851f7676380745d837cb4d6583d9951
bayk <konstantin.bay@gmail.com>
2020-06-11 16:51:58 -0700

    Format error message

--- src/main.rs
+++ src/main.rs
@@ -356,7 +356,7 @@ fn start_tor_listener(
 
                         Some(p)
                     },
-                    Err(e) => { cli_message!("error starting tor listener: {:?}", e); None},
+                    Err(e) => { cli_message!("Error: Unable to start tor listener: {}", e); None},
                 };
                 cli_message!("Tor listener has stopped.");
 
