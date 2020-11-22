f4f6c869b68c01240bddd8865a8451b4b43711ff
bayk <konstantin.bay@gmail.com>
2020-11-21 17:41:38 -0800

    Static openssl

--- build_static32.bat
+++ build_static32.bat
@@ -1,8 +1,8 @@
 cargo clean
 
-set OPENSSL_LIB_DIR="C:\Program Files (x86)\OpenSSL-Win32@1.1/lib/"
-set OPENSSL_INCLUDE_DIR="C:\Program Files (x86)\OpenSSL-Win32@1.1/include"
-set OPENSSL_STATIC="yes"
-set LIBCLANG_PATH="C:\Program Files (x86)\LLVM\bin\libclang.dll"
+set OPENSSL_LIB_DIR=C:\Program Files (x86)\OpenSSL-Win32/lib/
+set OPENSSL_INCLUDE_DIR=C:\Program Files (x86)\OpenSSL-Win32/include
+set OPENSSL_STATIC=yes
+set LIBCLANG_PATH=C:\Program Files (x86)\LLVM\bin\
 
-cargo +stable-i686-pc-windows-msvc build --release
+call .ci\win32_cargo.bat +stable-i686-pc-windows-msvc build --release
--- build_static64.bat
+++ build_static64.bat
@@ -1,8 +1,8 @@
 cargo clean
 
-set OPENSSL_LIB_DIR="C:\Program Files\OpenSSL-Win64@1.1/lib/"
-set OPENSSL_INCLUDE_DIR="C:\Program Files\OpenSSL-Win64@1.1/include"
-set OPENSSL_STATIC="yes"
-set LIBCLANG_PATH="C:\Program Files\LLVM\bin\libclang.dll"
+set OPENSSL_LIB_DIR=C:\Program Files\OpenSSL-Win64\lib\
+set OPENSSL_INCLUDE_DIR=C:\Program Files\OpenSSL-Win64\include
+set OPENSSL_STATIC=yes
+set LIBCLANG_PATH=C:\Program Files\LLVM\bin\libclang.dll
 
-cargo build --release
+call .ci\win64_cargo.bat build --release
