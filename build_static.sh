73699407bbfc11303c94668b5d4928b9c18fe319
bayk <konstantin.bay@gmail.com>
2021-01-27 18:37:24 -0800

    fixing the build

--- build_static.sh
+++ build_static.sh
@@ -4,6 +4,5 @@ cargo clean
 export OPENSSL_LIB_DIR=/usr/local/opt/openssl@1.1/lib/;
 export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@1.1/include;
 export OPENSSL_STATIC=yes
-export RUSTFLAGS="-Clink-arg=-Wl,--allow-multiple-definition"
 
 cargo build --release
